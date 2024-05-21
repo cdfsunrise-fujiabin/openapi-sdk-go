9c7ca17f6ba6ed1774ff8d3583ec9ea06434b805
cdfsunrise_fujiabin <fujiabin@cdfsunrise.com>
2024-05-21 15:30:17 +0800

    update

--- src/httpClient/qiyu_v_1_qiyu_v_2_order_list.go
+++ src/httpClient/qiyu_v_1_qiyu_v_2_order_list.go
@@ -9,54 +9,41 @@ import (
 )
 
 type QiYuOrderApiResult struct {
-	Data	QiYuOrderApiData
+	Data QiYuOrderApiData
 }
 
 type QiYuOrderApiData struct {
-	Banner	string `json:"banner"`
-	CurrentPage	int `json:"current_page"`
-	EmptyListHint	string `json:"empty_list_hint"`
-	Introducer	string `json:"introducer"`
-	PageSize	int `json:"page_size"`
-	Tab1	QiYuOrderApiTab
+	Banner        string `json:"banner"`
+	CurrentPage   int    `json:"current_page"`
+	EmptyListHint string `json:"empty_list_hint"`
+	Introducer    string `json:"introducer"`
+	PageSize      int    `json:"page_size"`
 }
-
-	Tab2	QiYuOrderApiTab
-}
-
-	Tab3	QiYuOrderApiTab
-}
-
-	Tab4	QiYuOrderApiTab
-}
-
-	TabId	int `json:"tab_id"`
-
 
 type QiYuOrderListQuery struct {
-	Params	any `json:"params"`
-	PlgCode	string `json:"plgCode"`
-	Token	string `json:"token"`
-	UserId	string `json:"userId"`
+	Params  any    `json:"params"`
+	PlgCode string `json:"plgCode"`
+	Token   string `json:"token"`
+	UserId  string `json:"userId"`
 }
 
 /*V1QiyuV2OrderList
  *Description: 订单列表查询
  * @param: body QiYuOrderListQuery QiYuOrderListQuery 必填项
  * @return: *QiYuOrderApiResult
-*/
+ */
 func (t *CdfSunriseRequestClient) V1QiyuV2OrderList(ctx context.Context, body QiYuOrderListQuery) (*QiYuOrderApiResult, error) {
 	headers := GenHeaders(nil)
-	
+
 	marshal, err := json.Marshal(body)
-    if err != nil {
-        return nil, err
-    }
-    respMap, err := exHttp.NewHttpRequest(ctx, t.host, fmt.Sprintf("/v1/qiyu/v2/order/list"), exHttp.WithHeaders(headers), exHttp.WithRequestBody(string(marshal))).PostUnmarshal()
-	
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
+	respMap, err := exHttp.NewHttpRequest(ctx, t.host, fmt.Sprintf("/v1/qiyu/v2/order/list"), exHttp.WithHeaders(headers), exHttp.WithRequestBody(string(marshal))).PostUnmarshal()
+
+	if err != nil {
+		return nil, err
+	}
 
 	var respEntity QiYuOrderApiResult
 	err = mapstructure.Decode(respMap, &respEntity)
